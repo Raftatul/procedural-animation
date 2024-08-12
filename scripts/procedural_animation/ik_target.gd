3afff012da06d7370e103ff24f1ee0a4f9566ec1
Raphael-Daubelcour <proraphael.daubelcour@gmail.com>
2024-08-12 16:58:11 -0700

    Change ik behaviour

--- scripts/procedural_animation/ik_target.gd
+++ scripts/procedural_animation/ik_target.gd
@@ -1,17 +1,28 @@
 class_name IKTarget
-extends RigidBody3D
+extends Marker3D
 
+
+const RAY_LENGTH = 1000
 
 @export var step_target: Node3D
+@export var ray: RayCast3D
 
+@export_group("IKs")
+@export var adjacent_target: IKTarget
+@export var opposite_target: IKTarget
+
+@export_group("Parameters")
 @export var front_step_distance := 3.0
 @export var step_height := 1.0
 @export var tween_speed := 0.1
 
-@export var adjacent_target: IKTarget
-@export var opposite_target: IKTarget
-
 var is_stepping := false
+
+
+func _process(delta: float) -> void:
+	var hit_point = ray.get_collision_point()
+	if hit_point:
+		global_position = hit_point
 
 
 func _physics_process(_delta: float) -> void:
@@ -29,5 +40,5 @@ func step() -> void:
 	var t = get_tree().create_tween()
 	t.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	t.tween_property(self, "global_position", half_way + owner.global_basis.y * step_height, tween_speed)
-	t.tween_property(self, "global_position", target_pos + Vector3.UP * 0.1, tween_speed)
+	t.tween_property(self, "global_position", target_pos, tween_speed)
 	t.tween_callback(func(): is_stepping = false)
