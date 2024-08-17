bf3cc52ea8b3d3c43039c8d11b25d9d2bf2423ae
Raphael-Daubelcour <proraphael.daubelcour@gmail.com>
2024-08-16 23:25:37 -0700

    Add sounds library

--- scripts/nodes/random_pitch_3D.gd
+++ scripts/nodes/random_pitch_3D.gd
@@ -2,6 +2,20 @@ class_name RandomPitch3D
 extends AudioStreamPlayer3D
 
 
-func play_stream(from_position: float = 0.0) -> void:
+@export var audio_library: AudioLibrary
+@export var custom_max_polyphonic := 32
+
+
+func _ready() -> void:
+	stream = AudioStreamPolyphonic.new()
+	stream.polyphony = custom_max_polyphonic
+
+
+func play_stream(tag:String, from_position: float = 0.0) -> void:
 	pitch_scale = randf_range(0.8, 1.2)
-	play(from_position)
+	
+	if !playing: play()
+	
+	var audio_stream := audio_library.get_audio_stream(tag)
+	var polyphonic_stream_playback := self.get_stream_playback()
+	polyphonic_stream_playback.play_stream(audio_stream)
