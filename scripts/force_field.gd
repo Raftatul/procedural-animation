b1347efd8da383c4b6aebcf469e25b933b6fc1db
Raphaël Daubelcour <daubelcour63@gmail.com>
2024-08-25 23:30:13 +0200

    Added bump sound

--- scripts/force_field.gd
+++ scripts/force_field.gd
@@ -9,4 +9,4 @@ func _physics_process(delta: float) -> void:
 		if body.get_meta("is_ball", false):
 			var direction_to_goal := body.global_position.direction_to($"../Area3D".global_position)
 			
-			body.apply_central_force(direction_to_goal * force * delta)
+			body.apply_central_force(direction_to_goal * force)
--- scripts/goal.gd
+++ scripts/goal.gd
@@ -1,11 +1,15 @@
 extends Area3D
 
 
+signal score
+
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.has_meta("is_ball"):
+		score.emit()
 		GlobalCanvasLayer.add_point(1)
 		body.queue_free()
