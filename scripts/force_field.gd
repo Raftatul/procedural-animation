bb8048f1607e7a059492fac49832acbfb1dd035a
Raphaël Daubelcour <daubelcour63@gmail.com>
2024-08-26 00:01:00 +0200

    Reworking force field

--- scripts/force_field.gd
+++ scripts/force_field.gd
@@ -7,6 +7,8 @@ extends Area3D
 func _physics_process(_delta: float) -> void:
 	for body in get_overlapping_bodies():
 		if body.get_meta("is_ball", false):
-			var direction_to_goal := body.global_position.direction_to($"../Area3D".global_position)
+			var goal_pos = $"../Area3D".global_position
+			var direction_to_goal := body.global_position.direction_to(goal_pos)
+			var distance = body.global_position.distance_to(goal_pos)
 			
-			body.apply_central_force(direction_to_goal * force)
+			body.apply_central_force(direction_to_goal * (force / distance))
